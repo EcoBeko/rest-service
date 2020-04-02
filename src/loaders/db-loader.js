044b012426689fb24583fc2b33678b594e18e767
RayDarar <dfqgth400@gmail.com>
2020-04-02 19:17:55 +0600

    mongoose removed

--- src/loaders/db-loader.js
+++ src/loaders/db-loader.js
@@ -1,9 +1,4 @@
-import mongoose from "mongoose";
 import config from "@/config";
 import pipe from "@/pipe";
 
-export async function init() {
-  mongoose.connection.on("open", () => pipe.emit("mongo::connected"));
-
-  await mongoose.connect(config.mongodb.uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-}
+export async function init() {}
